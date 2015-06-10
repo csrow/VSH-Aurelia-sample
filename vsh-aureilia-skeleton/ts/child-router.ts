0ea3d85d19076fee59d75c13dd79f327db170dea
csrow <csrow@hotmail.com>
2015-06-10 14:55:43 -0400

    Get Started 5 Child Router

---
+++ vsh-aureilia-skeleton/ts/child-router.ts
@@ -0,0 +1,13 @@
+﻿export class ChildRouter {
+  heading = 'Child Router';
+  router;
+  configureRouter(config, router) {
+    config.map([
+      { route: ['', 'welcome'], moduleId: './welcome', nav: true, title: 'Welcome' },
+      { route: 'flickr', moduleId: './flickr', nav: true, title: 'Flickr' },
+      { route: 'child-router', moduleId: './child-router', nav: true, title: 'Child Router' }
+    ]);
+
+    this.router = router;
+  }
+}
