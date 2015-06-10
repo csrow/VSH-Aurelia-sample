de5da5189779eda1709223dbb1c01bfcd79f9393
csrow <csrow@hotmail.com>
2015-06-09 20:48:03 -0400

    Get Started 1

---
+++ vsh-aureilia-skeleton/build/tasks/copyTS.js
@@ -0,0 +1,8 @@
+﻿var gulp = require('gulp');
+var paths = require('../paths');
+
+// move js and map files from ts to dist
+gulp.task('copyTS', function () {
+  gulp.src('ts/**/*.{html,js,map}')
+  .pipe(gulp.dest('dist'));
+});
---
+++ vsh-aureilia-skeleton/gulpfile.js
@@ -0,0 +1,4 @@
+/// <vs BeforeBuild='build' AfterBuild='copyTS' />
+// all gulp tasks are located in the ./build/tasks directory
+// gulp configuration is in files in ./build directory
+require('require-dir')('build/tasks');
